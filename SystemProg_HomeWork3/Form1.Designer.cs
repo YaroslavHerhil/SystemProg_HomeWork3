8ea56b9fe57f33aa11541a5839d8f3a350b92481
Gerg_hn60 <Gerg_hn60@student.itstep.org>
2023-10-28 15:06:03 +0200

    Add project files.

---
+++ SystemProg_HomeWork3/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SystemProg_HomeWork3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStop1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.buttonStart2 = new System.Windows.Forms.Button();
+            this.buttonStop2 = new System.Windows.Forms.Button();
+            this.buttonPause1 = new System.Windows.Forms.Button();
+            this.buttonPause2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart1
+            // 
+            this.buttonStart1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStart1.BackColor = System.Drawing.Color.YellowGreen;
+            this.buttonStart1.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.buttonStart1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart1.Location = new System.Drawing.Point(12, 381);
+            this.buttonStart1.Name = "buttonStart1";
+            this.buttonStart1.Size = new System.Drawing.Size(99, 56);
+            this.buttonStart1.TabIndex = 0;
+            this.buttonStart1.Text = "Start";
+            this.buttonStart1.UseVisualStyleBackColor = false;
+            this.buttonStart1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 114);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(333, 261);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(222, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 35);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(222, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 35);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Lower Boundary";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Upper Boundary";
+            // 
+            // buttonStop1
+            // 
+            this.buttonStop1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStop1.BackColor = System.Drawing.Color.LightCoral;
+            this.buttonStop1.Enabled = false;
+            this.buttonStop1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStop1.Location = new System.Drawing.Point(246, 381);
+            this.buttonStop1.Name = "buttonStop1";
+            this.buttonStop1.Size = new System.Drawing.Size(99, 56);
+            this.buttonStop1.TabIndex = 6;
+            this.buttonStop1.Text = "Stop";
+            this.buttonStop1.UseVisualStyleBackColor = false;
+            this.buttonStop1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(362, 114);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(333, 261);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // buttonStart2
+            // 
+            this.buttonStart2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart2.BackColor = System.Drawing.Color.YellowGreen;
+            this.buttonStart2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart2.Location = new System.Drawing.Point(362, 381);
+            this.buttonStart2.Name = "buttonStart2";
+            this.buttonStart2.Size = new System.Drawing.Size(99, 56);
+            this.buttonStart2.TabIndex = 8;
+            this.buttonStart2.Text = "Start";
+            this.buttonStart2.UseVisualStyleBackColor = false;
+            this.buttonStart2.Click += new System.EventHandler(this.buttonStart2_Click);
+            // 
+            // buttonStop2
+            // 
+            this.buttonStop2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop2.BackColor = System.Drawing.Color.LightCoral;
+            this.buttonStop2.Enabled = false;
+            this.buttonStop2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStop2.Location = new System.Drawing.Point(596, 381);
+            this.buttonStop2.Name = "buttonStop2";
+            this.buttonStop2.Size = new System.Drawing.Size(99, 56);
+            this.buttonStop2.TabIndex = 9;
+            this.buttonStop2.Text = "Stop";
+            this.buttonStop2.UseVisualStyleBackColor = false;
+            this.buttonStop2.Click += new System.EventHandler(this.buttonStop2_Click);
+            // 
+            // buttonPause1
+            // 
+            this.buttonPause1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPause1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonPause1.Enabled = false;
+            this.buttonPause1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPause1.Location = new System.Drawing.Point(117, 381);
+            this.buttonPause1.Name = "buttonPause1";
+            this.buttonPause1.Size = new System.Drawing.Size(123, 56);
+            this.buttonPause1.TabIndex = 10;
+            this.buttonPause1.Text = "Pause";
+            this.buttonPause1.UseVisualStyleBackColor = false;
+            this.buttonPause1.Click += new System.EventHandler(this.buttonPause1_Click);
+            // 
+            // buttonPause2
+            // 
+            this.buttonPause2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPause2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.buttonPause2.Enabled = false;
+            this.buttonPause2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPause2.Location = new System.Drawing.Point(467, 381);
+            this.buttonPause2.Name = "buttonPause2";
+            this.buttonPause2.Size = new System.Drawing.Size(123, 56);
+            this.buttonPause2.TabIndex = 11;
+            this.buttonPause2.Text = "Pause";
+            this.buttonPause2.UseVisualStyleBackColor = false;
+            this.buttonPause2.Click += new System.EventHandler(this.buttonPause2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 469);
+            this.Controls.Add(this.buttonPause2);
+            this.Controls.Add(this.buttonPause1);
+            this.Controls.Add(this.buttonStop2);
+            this.Controls.Add(this.buttonStart2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.buttonStop1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonStart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStop1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button buttonStart2;
+        private System.Windows.Forms.Button buttonStop2;
+        private System.Windows.Forms.Button buttonPause1;
+        private System.Windows.Forms.Button buttonPause2;
+    }
+}
+
---
+++ SystemProg_HomeWork3/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemProg_HomeWork3
+{
+    public partial class Form1 : Form
+    {
+        Thread primeNumberGeneratorThread = null;
+        Thread fibonacciNumberGeneratorThread = null;
+        ManualResetEvent mre1;
+        ManualResetEvent mre2;
+
+
+        public Form1()
+        {
+            mre1 = new ManualResetEvent(true);
+            mre2 = new ManualResetEvent(true);
+            InitializeComponent();
+        }
+
+
+        private void GeneratePrimeNumbers(object parameters)
+        {
+            int[] boundaries = (int[])parameters;
+            int head = boundaries[0];
+            int tail = boundaries[1];
+
+            int numberBuffer = head;
+            while (numberBuffer <= tail || tail < 0) 
+            {
+                bool isPrime = true;
+                int divisorCounter = 0;
+
+                mre1.WaitOne();
+                for (int i = 1; i <= numberBuffer; i++) {
+                    if (numberBuffer % i == 0)
+                        divisorCounter++;
+                    if(divisorCounter > 2)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    BeginInvoke(new Action(() => { richTextBox1.AppendText(numberBuffer + " ");}));
+                    
+                }
+                Thread.Sleep(1);
+
+                numberBuffer++;
+            }
+
+            BeginInvoke(new Action(() =>
+            {
+                buttonStop1.Enabled = false;
+                buttonStart1.Enabled = true;
+                primeNumberGeneratorThread.Abort();
+            }));
+        }
+        private void GenerateFibonacciNumbers(object parameters)
+        {
+            int[] boundaries = (int[])parameters;
+            int head = boundaries[0];
+            int tail = boundaries[1];
+
+            int twoStepsBehind = 0;
+            int oneStepBehind = head;
+
+            int numberBuffer = 0;
+            while ((numberBuffer <= tail && numberBuffer >= 0 || tail < 0 ) && numberBuffer >= 0)
+            {
+                mre2.WaitOne();
+                numberBuffer = oneStepBehind + twoStepsBehind;
+
+                BeginInvoke(new Action(() => { richTextBox2.AppendText(numberBuffer + " "); }));
+
+                
+                Thread.Sleep(1);
+                twoStepsBehind = oneStepBehind;
+                oneStepBehind = numberBuffer;
+                
+            }
+
+            BeginInvoke(new Action(() =>
+            {
+                buttonStop2.Enabled = false;
+                buttonStart2.Enabled = true;
+                fibonacciNumberGeneratorThread.Abort();
+            }));
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonStop1.Enabled = true;
+            buttonPause1.Enabled = true;
+            buttonStart1.Enabled = false;
+
+            richTextBox1.Clear();
+
+            primeNumberGeneratorThread = new Thread(new ParameterizedThreadStart(GeneratePrimeNumbers));
+
+            if (!int.TryParse(textBox1.Text, out int lowerB) || lowerB <= 0)
+                lowerB = 2;
+            if (!int.TryParse(textBox2.Text, out int upperB))
+                upperB = -1;
+
+
+
+            int[] parameters = { lowerB, upperB };
+            primeNumberGeneratorThread.Start(parameters);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            buttonStop1.Enabled = false;
+            buttonStart1.Enabled = true;
+            buttonPause1.Enabled = false;
+
+            primeNumberGeneratorThread.Abort();
+        }
+
+        private void buttonStart2_Click(object sender, EventArgs e)
+        {
+            buttonStop2.Enabled = true;
+            buttonStart2.Enabled = false;
+            buttonPause2.Enabled = true;
+
+
+            fibonacciNumberGeneratorThread = new Thread(new ParameterizedThreadStart(GenerateFibonacciNumbers));
+
+            richTextBox2.Clear();
+
+            if (!int.TryParse(textBox1.Text, out int lowerB) || lowerB <= 0)
+                lowerB = 2;
+            if (!int.TryParse(textBox2.Text, out int upperB))
+                upperB = -1;
+
+            int[] parameters = { lowerB, upperB };
+            fibonacciNumberGeneratorThread.Start(parameters);
+
+        }
+
+        private void buttonStop2_Click(object sender, EventArgs e)
+        {
+            buttonStop2.Enabled = false;
+            buttonStart2.Enabled = true;
+            buttonPause2.Enabled = false;
+
+            fibonacciNumberGeneratorThread.Abort();
+        }
+
+        private void buttonPause1_Click(object sender, EventArgs e)
+        {
+            if (mre1.WaitOne(0))
+            {
+                mre1.Reset();
+                buttonPause1.Text = "Resume";
+            }
+            else
+            {
+                mre1.Set();
+                buttonPause1.Text = "Pause";
+            }
+        }
+
+        private void buttonPause2_Click(object sender, EventArgs e)
+        {
+            if (!mre2.WaitOne(0))
+            {
+                mre2.Reset();
+                buttonPause2.Text = "Resume";
+            }
+            else
+            {
+                mre2.Set();
+                buttonPause2.Text = "Pause";
+            }
+        }
+    }
+}
